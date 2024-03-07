540e1db3f8538fac294182498356a07572c01125
Ludo Visser <ludo.visser@gmail.com>
2024-03-07 21:04:47 +0100

    Move calibration into controllers

--- truck01.py
+++ truck01.py
@@ -27,7 +27,7 @@ LOOP_DELAY = 100  # ms
 SHUTDOWN_LIMIT = 20  # x LOOP_DELAY ms
 
 hub = TechnicHub()
-print(f"Hub: {hub.system.name()}")
+print(f"hub: {hub.system.name()}")
 hub.light.animate([v for _, v in MODES_COLORS.items()], interval=500)
 
 remote = None
@@ -56,55 +56,114 @@ def toggle_mode(current_mode, modes):
     return modes[(idx + 1) % len(modes)]
 
 
-def calibrate(func, color=Color.WHITE):
+def calibrate(calibration_fcn, color=Color.WHITE):
     hub.light.blink(color, [200, 300])
     remote.light.on(color)
-    # func()
-    wait(5000)
+    calibration_fcn()
     hub.light.off()
     remote.light.off()
 
 
-
 # Controllers
 class DriveControl:
-    def __init__(self, port_drive, port_steering):
-        pass
+    def __init__(self):
+        self._motor_drive = Motor(PORT_DRIVE, Direction.COUNTERCLOCKWISE, gears=[20, 28])
+        self._motor_steering = Motor(PORT_STEERING, Direction.CLOCKWISE)
     
     def calibrate_drive(self):
-        wait(5000)
+        print("drive: forward")
+        self._motor_drive.run_time(speed=100.0, time=2000.0, then=Stop.BRAKE)
+        wait(500)
+        
+        print("drive: backward")
+        self._motor_drive.run_time(speed=-100.0, time=2000.0, then=Stop.BRAKE)
+        wait(500)
     
     def calibrate_steering(self):
-        wait(5000)
+        print("steering: right")
+        angle_right = self._motor_steering.run_until_stalled(speed=100.0, then=Stop.HOLD, duty_limit=25)
+        wait(500)
+        
+        print("steering: left")
+        angle_left = self._motor_steering.run_until_stalled(speed=-100.0, then=Stop.HOLD, duty_limit=25)
+        wait(500)
+
+        # We moved right to left and left < right because we moved
+        # counter-clockwise to go left, so choosing the center at
+        # d = (left - right) / 2 puts the center at 0 if we reset while at the
+        # left position
+        angle_center = (angle_left - angle_right) / 2.0
+        print(f"  right={angle_right}, left={angle_left}, center={angle_center}")
+        
+        print("steering: reset")
+        self._motor_steering.reset_angle(angle_center)
+        self._motor_steering.run_target(speed=100.0, target_angle=0.0)
+        wait(500)
     
-    def process(self, buttons):
+    def process(self, mode, buttons):
         pass
     
 
 class FunctionControl:
-    def __init__(self, port_coupling, port_aux):
-        pass
-    
+    def __init__(self):
+        self._motor_coupling = Motor(PORT_COUPLING, Direction.COUNTERCLOCKWISE)
+        self._motor_aux = Motor(PORT_AUX, Direction.CLOCKWISE)
+
     def calibrate_coupling(self):
-        wait(5000)
+        # Ratio motor->coupler = 12/12 * 12/24 * 16/16 * 1/8 = 8/8
+        ratio = (12 / 24) * (1 / 8)
+
+        # Assume the coupling was closed, then opening is 90 degrees further; to
+        # calibrate we overshoot to make sure we reach the limits (if it was
+        # actually open already, it'll slip)
+        assumed_angle_closed = self._motor_coupling.angle()
+        assumed_angle_open = int(assumed_angle_closed + 100 / ratio)
+        print(f"  assume: closed={assumed_angle_closed}, open={assumed_angle_open}")
+        
+        print("coupling: open")
+        self._motor_coupling.run_target(speed=200.0, target_angle=assumed_angle_open, then=Stop.HOLD)
+        wait(500)
+
+        # Now the coupling should be open
+        self._angle_open = self._motor_coupling.angle()
+        self._angle_closed = int(self._angle_open - 90 / ratio)
+        print(f"  actual: closed={self._angle_closed}, open={self._angle_open}")
+
+        print("coupling: close")
+        self.motor_coupling.run_target(speed=200.0, target_angle=self._angle_closed, then=Stop.HOLD)
+        wait(500)
 
     def calibrate_aux(self):
-        wait(5000)
+        print("aux: clockwise")
+        self._motor_aux.run_time(speed=300.0, time=2000.0, then=Stop.BRAKE)
+        wait(500)
+
+        print("aux: counter-clockwise")
+        self._motor_aux.run_time(speed=-300.0, time=2000.0, then=Stop.BRAKE)
+        wait(500)
+
+    def process(self, mode, buttons):
+        pass
     
 
 # Connect remote
 try:
     remote = Remote(name=REMOTE_ID)
-    print(f"Remote connected: {remote.name()}")
+    print(f"remote connected: {remote.name()}")
 except OSError:
     halt()
 
 remote.light.off()
+for _ in range(10):
+    remote.light.on(Color.ORANGE)
+    wait(200)
+    remote.light.off()
+    wait(200)
 
 
 # Instantiate controllers
-drive_control = DriveControl(port_drive=PORT_DRIVE, port_steering=PORT_STEERING)
-function_control = FunctionControl(port_coupling=PORT_COUPLING, port_aux=PORT_AUX)
+drive_control = DriveControl()
+function_control = FunctionControl()
 
 # Calibration
 calibrations = [
@@ -113,126 +172,53 @@ calibrations = [
     ("coupling", function_control.calibrate_coupling, Color.WHITE),
     ("auxiliary", function_control.calibrate_aux, Color.ORANGE)
 ]
-while calibrations:
-    while True:
-        buttons = remote.buttons.pressed()
-        if Button.CENTER in buttons:
-            break
-        wait(LOOP_DELAY)
 
-    s, f, c = calibrations.pop()
-    print(f"calibrating: {s}")
-    calibrate(f, c)
+try:
+    while calibrations:
+        while True:
+            buttons = remote.buttons.pressed()
+            if Button.CENTER in buttons:
+                break
+            wait(LOOP_DELAY)
+
+        name, calibration_fcn, color = calibrations.pop()
+        print(f"calibrating: {name}")
+        calibrate(calibration_fcn, color)
+except OSError as e:
+    print("error while calibrating")
+    print(e)
+    halt()
 
 
 # Event loop
 current_mode = MODE_INVALID
 new_mode = MODE_IDLE
 shutdown_count = 0
-while True:
-    if current_mode != new_mode:
-        current_mode = new_mode
-        c = MODES_COLORS[current_mode]
-        remote.light.on(c)
-        hub.light.on(c)
-    
-    buttons = remote.buttons.pressed()
-    if buttons == (Button.LEFT, Button.RIGHT):
-        new_mode = MODE_SHUTDOWN
-    elif buttons == (Button.CENTER, ):
-        new_mode = toggle_mode(current_mode, [MODE_IDLE, MODE_DRIVE, MODE_FUNCTION])
-    else:
-        pass  # parsing of buttons is done later
+try:
+    while True:
+        if current_mode != new_mode:
+            current_mode = new_mode
+            c = MODES_COLORS[current_mode]
+            remote.light.on(c)
+            hub.light.on(c)
+        
+        buttons = remote.buttons.pressed()
+        if buttons == (Button.LEFT, Button.RIGHT):
+            new_mode = MODE_SHUTDOWN
+        elif buttons == (Button.CENTER, ):
+            new_mode = toggle_mode(current_mode, [MODE_IDLE, MODE_DRIVE, MODE_FUNCTION])
+        else:
+            pass  # parsing of buttons is done by controllers
 
-    if current_mode == MODE_IDLE:
-        pass
-    elif current_mode == MODE_DRIVE:
-        pass
-    elif current_mode == MODE_FUNCTION:
-        pass
-    elif current_mode == MODE_SHUTDOWN:
-        shutdown_count += 1
-        if shutdown_count == SHUTDOWN_LIMIT:
-            hub.system.shutdown()
-    else:
-        halt()
+        if current_mode == MODE_SHUTDOWN:
+            shutdown_count += 1
+            if shutdown_count == SHUTDOWN_LIMIT:
+                hub.system.shutdown()
+        else:
+            shutdown_count = 0
 
-    wait(LOOP_DELAY)
-
-
-
-
-# Driving
-motor_drive = Motor(
-    port=PORT_DRIVE,
-    positive_direction=Direction.COUNTERCLOCKWISE,
-    gears=[20, 28],
-    reset_angle=False
-)
-
-print("DRIVE: forward")
-#motor_drive.run_time(speed=100.0, time=2000.0, then=Stop.COAST)
-wait(500)
-print("DRIVE: backward")
-#motor_drive.run_time(speed=-100.0, time=2000.0, then=Stop.COAST)
-wait(500)
-
-# Steering
-motor_steering = Motor(
-    port=PORT_STEERING,
-    positive_direction=Direction.CLOCKWISE,
-    reset_angle=False
-)
-
-print("STEER: right")
-#angle_right = motor_steering.run_until_stalled(speed=100.0, then=Stop.BRAKE, duty_limit=25)
-#print(f"angle_right={angle_right}")
-wait(500)
-print("STEER: left")
-#angle_left = motor_steering.run_until_stalled(speed=-100.0, then=Stop.BRAKE, duty_limit=25)
-#print(f"angle_right={angle_left}")
-wait(500)
-
-print("STEER: reset")
-#angle_center = (angle_left - angle_right) / 2.0  # must be CW - CCW!
-#print(f"center={angle_center}")
-#motor_steering.reset_angle(angle_center)
-print("STEER: center")
-#motor_steering.run_target(speed=100.0, target_angle=0.0)
-wait(500)
-
-# Coupling
-motor_coupling = Motor(
-    port=PORT_COUPLING,
-    positive_direction=Direction.COUNTERCLOCKWISE,
-    gears=[12, 24],
-    reset_angle=False
-)
-
-# ratio motor->coupler = 12/12 * 12/24 * 16/16 * 1/8
-angle_close = motor_coupling.angle()
-angle_open = int(angle_close + 90 / (12 / 24 * (1 / 8)))
-print("COUPLING: open")
-print(f"angle_open={angle_open}")
-#motor_coupling.run_target(speed=200.0, target_angle=angle_open, then=Stop.HOLD)
-wait(500)
-print("COUPLING: close")
-#motor_coupling.run_target(speed=200.0, target_angle=angle_close, then=Stop.HOLD)
-print(f"angle_close={angle_close}")
-wait(500)
-
-# Auxiliary
-motor_aux = Motor(
-    port=PORT_AUX,
-    positive_direction=Direction.CLOCKWISE,
-    reset_angle=False
-)
-
-print("AUX: clockwise")
-#motor_aux.run_time(speed=300.0, time=2000.0, then=Stop.COAST)
-wait(500)
-print("AUX: counter-clockwise")
-#motor_aux.run_time(speed=-300.0, time=2000.0, then=Stop.COAST)
-wait(500)
-
-print("STOP")
+        wait(LOOP_DELAY)
+except OSError as e:
+    print("error in eventloop")
+    print(e)
+    halt()
